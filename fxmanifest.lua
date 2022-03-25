df3d7a3fc205698188162aa75a8a01c7ba576885
Nightt7 <101990128+Nightt7@users.noreply.github.com>
2022-03-26 00:59:13 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version "adamant"
+game "gta5"
+
+name "mumble-voip"
+author "Nightt#8899 Modified."
+
+ui_page "ui/index.html"
+
+files {
+	"ui/index.html",
+	"/ui/franklin.ttf",
+	"ui/mic_click_on.ogg",
+	"ui/mic_click_off.ogg",
+}
+
+shared_scripts {
+	"config.lua",
+	"grid.lua",
+}
+
+client_scripts {
+	"client.lua",
+}
+
+server_scripts {
+	"server.lua",
+}
